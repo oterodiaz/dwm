9c99f3af78adbad1a8b90b1fe734bd2cc4be6aa3
oterodiaz <44239644+oterodiaz@users.noreply.github.com>
2020-12-03 23:35:10 +0100

    Apply patch: bar height

--- config.def.h
+++ config.def.h
@@ -11,6 +11,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 0;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const int horizpadbar        = 2;        /* horizontal padding for statusbar */
 static const int vertpadbar         = 0;        /* vertical padding for statusbar */
 static const unsigned int baralpha = 0xd0; /*0xff is opaque*/
