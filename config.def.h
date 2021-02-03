4b055ca026f840258efabbb507c5582c8a362a3b
oterodiaz <44239644+oterodiaz@users.noreply.github.com>
2021-02-03 16:00:00 +0100

    Make Firefox rule lowercase

--- config.def.h
+++ config.def.h
@@ -51,7 +51,7 @@ static const Rule rules[] = {
 	/* class          instance  title           tags mask  isfloating  isterminal  noswallow  monitor  fullscreen*/
 	{ "Gimp",         NULL,     NULL,           0,         1,          0,           0,        -1,           0 },
 	{ "Telegram",     NULL,     NULL,           ~0,        1,          0,           0,        -1,           0 },
-	{ "Firefox",      NULL,     NULL,           1 << 3,    0,          0,          -1,        -1,           0 },
+	{ "firefox",      NULL,     NULL,           1 << 3,    0,          0,          -1,        -1,           0 },
 	{ "Alacritty",    NULL,     NULL,           0,         0,          1,           0,        -1,           0 },
 	{ NULL,           NULL,     "Arcolinux Logout",    0,         0,          0,           1,        -1,           1 },
 	{ NULL,           NULL,     "Event Tester", 0,         0,          0,           1,        -1,           0 }, /* xev */
