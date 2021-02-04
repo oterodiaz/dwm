1d130bffc126e41b6fd72014b529501f75d5ed98
oterodiaz <44239644+oterodiaz@users.noreply.github.com>
2021-02-04 22:40:43 +0100

    Make the Tile layout the default again

--- config.def.h
+++ config.def.h
@@ -65,13 +65,13 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
- 	{ "[\\]",     dwindle },    /* first entry is default */
+	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 	{ "|M|",      centeredmaster },
 	{ ">M>",      centeredfloatingmaster },
  	{ "[@]",      spiral },
-	{ "[]=",      tile },
+ 	{ "[\\]",     dwindle },
 };
 
 /* key definitions */
