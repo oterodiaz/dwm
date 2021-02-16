3e349009cedcb7be1bbfbe369760cf5ee468a6a6
oterodiaz <44239644+oterodiaz@users.noreply.github.com>
2021-02-16 18:32:01 +0100

    Add new rule

--- config.def.h
+++ config.def.h
@@ -51,6 +51,7 @@ static const Rule rules[] = {
 	/* class          instance  title           tags mask  isfloating  isterminal  noswallow  monitor  fullscreen*/
 	{ "Telegram",     NULL,     NULL,           ~0,        1,          0,           0,        -1,           0 },
 	{ "firefox",      NULL,     NULL,           1 << 3,    0,          0,          -1,        -1,           0 },
+	{ "FreeTube",      NULL,     NULL,           1 << 3,    0,          0,          -1,        -1,           0 },
 	{ "Alacritty",    NULL,     NULL,           0,         0,          1,           0,        -1,           0 },
 	{ NULL,           NULL,     "Arcolinux Logout",    0,         0,          0,           1,        -1,           1 },
 	{ NULL,           NULL,     "Event Tester", 0,         0,          0,           1,        -1,           0 }, /* xev */
