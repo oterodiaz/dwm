de9e27422cecd00e7e79b04fee014709d3471f03
oterodiaz <44239644+oterodiaz@users.noreply.github.com>
2021-03-16 17:30:20 +0100

    Finish organizing the whole config

--- config.def.h
+++ config.def.h
@@ -1,54 +1,65 @@
 /* See LICENSE file for copyright and license details. */
 
-/* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 10};
-static const unsigned int snap      = 5;       /* snap pixel */
-static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static int showsystray              = 1;        /* 0 means no systray */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh            = 25;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const int horizpadbar        = 2;        /* horizontal padding for statusbar */
-static const int vertpadbar         = 0;        /* vertical padding for statusbar */
-static const unsigned int baralpha = 0x44;      /*0xff is opaque*/
-static const unsigned int borderalpha = OPAQUE;
-static const char *fonts[]          = { "SFMono Nerd Font:size=11:style=Regular", "Apple Color Emoji:size=11" };
-static const char dmenufont[]       = "SFMono Nerd Font:size=11:style=Regular";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#9F0BD9";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+/* Borders and gaps */
+static const Gap          default_gap     = \
+{.isgap = 1, .realgap = 10, .gappx = 10};           // Gaps size in pixels (.realgap and .gappx should be equal)
+static const unsigned int borderpx        = 2;      // Window border width in pixels
+static const unsigned int snap            = 5;      // Snap floating windows to the edge of the screen when 'snap' pixels away
+static const unsigned int borderalpha     = OPAQUE; // Fix window borders being transparent, don't touch this
+
+/* Statusbar and systray */
+static const int          topbar                  = 1;    // 0: Bottom bar, 1: Top bar
+static const int          showbar                 = 1;    // 0: Hide bar, 1: Show bar
+static int                showsystray             = 1;    // 0: Hide systray, 1: Show systray
+static const int          user_bh                 = 25;   // Bar height in pixels (0: Calculate bar height, >=1: user_bh as bar height)
+static const int          horizpadbar             = 2;    // Horizontal padding for statusbar in pixels
+static const int          vertpadbar              = 0;    // Vertical padding for statusbar in pixels
+static const unsigned int systrayspacing          = 2;    // Systray spacing in pixels
+static const unsigned int baralpha                = 0x44; // Bar opacity in Hex (0xff is opaque)
+static const unsigned int systraypinning          = 0;    // 0: Sloppy systray follows selected monitor, >0: Pin systray to monitor X
+static const int          systraypinningfailfirst = 1;    // 0: display systray on the last monitor, 1: If pinning fails, display systray on the first monitor
+
+/* Swallow */
+static const int swallowfloating = 0;   // 0: Do nothing, 1: Swallow floating windows by default
+
+/* Fonts */
+static const char *fonts[]    = { "SFMono Nerd Font:size=11:style=Regular", "Apple Color Emoji:size=11" };
+static const char dmenufont[] = "SFMono Nerd Font:size=11:style=Regular";
+
+/* Colors */
+static const char col_gray1[]  = "#222222";
+static const char col_gray2[]  = "#444444";
+static const char col_gray3[]  = "#bbbbbb";
+static const char col_gray4[]  = "#eeeeee";
+static const char col_cyan[]   = "#9F0BD9"; // Accent
+static const char *colors[][3] = {
+	/* Format: Foreground, Background, Border */
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 }, // Regular windows
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  }, // Focused windows
 };
 static const unsigned int alphas[][3]      = {
-	/*               fg      bg        border     */
 	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
 	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
+/* Autostart */
 static const char *const autostart[] = {
 	"/home/diego/.config/dwm/autostart.sh", NULL,
     "dwmblocks", NULL,
-	NULL /* terminate */
+	NULL /* Terminate */
 };
 
-/* tagging */
+/* Tags */
 static const char *tags[] = { "", "", "", "" };
 
+/* Rules */
 static const Rule rules[] = {
 	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
+	  	WM_CLASS(STRING) = instance, class
+	  	WM_NAME(STRING) = title
 	 */
-	/* class          instance  title               tags mask  isfloating  isterminal  noswallow  monitor  fullscreen*/
+
+	/* Class          Instance  Title               Tags mask  isfloating  isterminal  noswallow  monitor      fullscreen*/
 	{ "Telegram",     NULL,     NULL,               ~0,        1,          0,           0,        -1,           0 },
 	{ "firefox",      NULL,     NULL,               1 << 3,    0,          0,          -1,        -1,           0 },
 	{ "Brave-browser",NULL,     NULL,               1 << 3,    0,          0,          -1,        -1,           0 },
@@ -57,20 +68,19 @@ static const Rule rules[] = {
 	{ "Pcmanfm",      NULL,     NULL,               0,         0,          0,           1,        -1,           0 },
 	{ "Alacritty",    NULL,     NULL,               0,         0,          1,           0,        -1,           0 },
 	{ NULL,           NULL,     "Arcolinux Logout", 0,         0,          0,           1,        -1,           1 },
-	{ NULL,           NULL,     "Event Tester",     0,         0,          0,           1,        -1,           0 }, /* xev */
+	{ NULL,           NULL,     "Event Tester",     0,         0,          0,           1,        -1,           0 }, // xev
 	/* { "Firefox",   NULL,     NULL,               1 << 8,    0,          0,          -1,        -1            0 }, */
 	/* { "St",        NULL,     NULL,               0,         0,          1,           0,        -1            0 }, */
 };
 
-/* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
-
+/* Layouts */
+static const float mfact     = 0.55; // Factor of master area size [0.05..0.95] (Vertical split position)
+static const int nmaster     = 1;    // Number of windows in the master stack
+static const int resizehints = 1;    // 1 means respect size hints in tiled resizals
 static const Layout layouts[] = {
-	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	/* Symbol     Arrange function */
+	{ "[]=",      tile },    // First entry is default
+	{ "><>",      NULL },    // No layout function means floating behavior
 	{ "[M]",      monocle },
 	{ "|M|",      centeredmaster },
 	{ ">M>",      centeredfloatingmaster },
@@ -78,13 +88,13 @@ static const Layout layouts[] = {
  	{ "[\\]",     dwindle },
 };
 
-/* key definitions */
+/* Key definitions */
 #define MODKEY Mod4Mask
 #define TAGKEYS(CHAIN,KEY,TAG) \
-	{ MODKEY,                       CHAIN, KEY,      view,           {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask,           CHAIN, KEY,      toggleview,     {.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask,             CHAIN, KEY,      tag,            {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask|ShiftMask, CHAIN, KEY,      toggletag,      {.ui = 1 << TAG} },
+	{ MODKEY,                       CHAIN, KEY, view,       {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,           CHAIN, KEY, toggleview, {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             CHAIN, KEY, tag,        {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask|ShiftMask, CHAIN, KEY, toggletag,  {.ui = 1 << TAG} },
 #define STACKKEYS(MOD,ACTION) \
 	{ MOD, -1, XK_j,     ACTION##stack, {.i = INC(+1) } }, \
 	{ MOD, -1, XK_k,     ACTION##stack, {.i = INC(-1) } }, \
@@ -94,19 +104,21 @@ static const Layout layouts[] = {
 	/* { MOD, -1, XK_z,     ACTION##stack, {.i = 1 } }, \ */
 	/* { MOD, -1, XK_x,     ACTION##stack, {.i = 2 } }, \ */
 
-/* helper for spawning shell commands in the pre dwm-5.0 fashion */
+/* Helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
-/* commands */
-static char dmenuopacity[4] = "0.65"; /* Values from 0 (transparent) to 1 (opaque) */
-static char dmenudim[4] = "0"; /* Values from 0 (transparent) to 1 (opaque) */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static char dmenuheight[3] = "25";
-static const char *dmenucmd[] = { "dmenu_run", "-s", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-o", dmenuopacity, "-dim", dmenudim, "-h", dmenuheight, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+/* Commands */
+static char        dmenumon[2]     = "0";        // component of dmenucmd, manipulated in spawn()
+static char        dmenuheight[3]  = "25";       // dmenu height
+static char        dmenuopacity[4] = "0.65";     // Values from 0 (transparent) to 1 (opaque)
+static char        dmenudim[4]     = "0";        // Values from 0 (transparent) to 1 (opaque)
+static const char *termcmd[]       = { "alacritty", NULL };
+static const char *dmenucmd[]      = \
+{ "dmenu_run", "-s", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-o", dmenuopacity, "-dim", dmenudim, "-h", dmenuheight, NULL };
 
+/* Keybindings */
 static Key keys[] = {
-	/*  Format: Modifier, Key chain, Key, Function, Argument */
+	/* Format: Modifier, Key chain, Key, Function, Argument */
 
     /* Misc. */
 	{ MODKEY,            -1, XK_Return, spawn, {.v = termcmd } },                           // Terminal
@@ -162,6 +174,10 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,  -1, XK_BackSpace, quit,              {0} },               // Quit dwm
 	{ MODKEY|ShiftMask,  -1, XK_r,         quit,              {1} },               // Restart dwm
 
+    /* Stacker */
+	STACKKEYS(MODKEY, focus)
+	STACKKEYS(MODKEY|ShiftMask, push)
+
     /* Tags */
 	TAGKEYS( -1, XK_1, 0) // First tag
 	TAGKEYS( -1, XK_2, 1) // Second tag
@@ -200,10 +216,6 @@ static Key keys[] = {
 	{ MODKEY, -1, XK_F2, spawn, SHCMD("redshift -x && fish -c 'set -Ux REDSHIFT_ON false' && kill -45 $(pidof dwmblocks)") },        // Disable redshift
 	{ MODKEY, -1, XK_F1, spawn, SHCMD("redshift -P -O 3500 && fish -c 'set -Ux REDSHIFT_ON true' && kill -45 $(pidof dwmblocks)") }, // Enable redshift
 
-    /* Stacker */
-	STACKKEYS(MODKEY, focus)
-	STACKKEYS(MODKEY|ShiftMask, push)
-
     /* Unused keybindings */
 	/* { MODKEY,           XK_z,      zoom,      {0} }, */
 	/* { MODKEY,           XK_space,  setlayout, {0} }, */
@@ -218,8 +230,8 @@ static Key keys[] = {
 	/* TAGKEYS(            XK_9,                 8) */
 };
 
-/* button definitions */
-/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+/* Button definitions */
+/* Click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
 	/* Click         Event mask  Button   Function        Argument */
 	{ ClkLtSymbol,   0,          Button1, setlayout,      {0} },
@@ -235,9 +247,9 @@ static Button buttons[] = {
 	{ ClkTagBar,     MODKEY,     Button3, toggletag,      {0} },
 };
 
-/* signal definitions */
+/* Signals */
 /* signum must be greater than 0 */
-/* trigger signals using `xsetroot -name "fsignal:<signum>"` */
+/* trigger signals using 'xsetroot -name "fsignal:<signum>"' */
 static Signal signals[] = {
 	/* Signum  Function       Argument  */
 	{ 1,       togglefullscr, {0} },
