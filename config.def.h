0a99e190e24aef2d526f48d909aea7858b721781
oterodiaz <44239644+oterodiaz@users.noreply.github.com>
2021-04-20 18:57:22 +0200

    Create/remove temporary file when toggling redshift

--- config.def.h
+++ config.def.h
@@ -219,8 +219,8 @@ static Key keys[] = {
     { MODKEY,    -1, 0x1008ff03, spawn, SHCMD("/home/diego/.scripts/brightness.py -s 50 && kill -45 $(pidof dwmblocks)") },  // 50%
 
     /* Blue Light Filter */
-    { MODKEY, -1, XK_F2, spawn, SHCMD("redshift -x && fish -c 'set -Ux REDSHIFT_ON false' && kill -45 $(pidof dwmblocks)") },        // Disable redshift
-    { MODKEY, -1, XK_F1, spawn, SHCMD("redshift -P -O 3500 && fish -c 'set -Ux REDSHIFT_ON true' && kill -45 $(pidof dwmblocks)") }, // Enable redshift
+    { MODKEY, -1, XK_F2, spawn, SHCMD("redshift -x && rm /tmp/redshift.on  && kill -45 $(pidof dwmblocks)") },        // Disable redshift
+    { MODKEY, -1, XK_F1, spawn, SHCMD("redshift -P -O 3500 && touch /tmp/redshift.on && kill -45 $(pidof dwmblocks)") }, // Enable redshift
 
     /* Unused keybindings */
     /* { MODKEY,           XK_z,      zoom,      {0} }, */
